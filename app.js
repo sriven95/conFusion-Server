5fdee0bf7bc88dad32e3831de2d78013bb8ba84e
sriven95 <srivenbommakanti@gmail.com>
2020-07-22 15:41:05 +0530

    Express session part 2

--- app.js
+++ app.js
@@ -39,43 +39,26 @@ app.use(session({
   saveUninitialized:false,
   resave:false,
   store:new FileStore()
-}))
-function auth(req,res,next){
-  console.log(req.session);
-  if(!req.session.user){
-    var authHeader = req.headers.authorization;
-    if(!authHeader){
-      var err = new Error('Your are not authenticated!');
-      
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status =401;
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
-
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');//base Qww566:8553
-    var username = auth[0];
-    var password = auth[1];
-
-    if(username === 'admin' && password === 'password'){
-      req.session.user = 'admin';
-      next();//passes to next setoff middle-ware
-    }
-    else{
-      var err = new Error('Your are not authenticated!');
-      
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status =401;
-      return next(err);
-    }
   }
-  else{
-    if(req.session.user === 'admin'){
+  else {
+    if (req.session.user === 'authenticated') {
       next();
     }
-    else{
-      var err = new Error('Your are not authenticated!');      
-      err.status =401;
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
       return next(err);
     }
   }
@@ -85,8 +68,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
