c541c2e03ace811c1501e7fbefb7bc7d54e6ebd4
sriven95 <srivenbommakanti@gmail.com>
2020-07-22 12:10:15 +0530

    Basic Auth

--- app.js
+++ app.js
@@ -30,6 +30,38 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res,next){
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  if(!authHeader){
+    var err = new Error('Your are not authenticated!');
+    
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status =401;
+    return next(err);
+
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');//base Qww566:8553
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password'){
+    next();//passes to next setoff middle-ware
+  }
+  else{
+    var err = new Error('Your are not authenticated!');
+    
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status =401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
